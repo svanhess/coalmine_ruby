bffc1039ff5c4f19e9230b757f60ff978e839eb8
Brad Seefeld <bradley.seefeld@gmail.com>
2012-04-09 20:02:19 -0700

    Minor refactoring

--- lib/coalmine/configuration.rb
+++ lib/coalmine/configuration.rb
@@ -1,37 +1,39 @@
-class Coalmine::Configuration
+module Coalmine
+  class Configuration
   
-  attr_accessor :url, :environment, :signature, :logger, :host, :port, :proxy_host, 
-      :proxy_port, :proxy_user, :proxy_password, :protocol, :secure, :http_open_timeout,
-      :http_read_timeout, :project_root, :framework, :filters,
+    attr_accessor :url, :environment, :signature, :logger, :host, :port, :proxy_host, 
+        :proxy_port, :proxy_user, :proxy_password, :protocol, :secure, :http_open_timeout,
+        :http_read_timeout, :project_root, :framework, :filters,
       
-      # For HTTP basic auth
-      :http_user, :http_password,
+        # For HTTP basic auth
+        :http_user, :http_password,
       
-      # The application version
-      :version,
+        # The application version
+        :version,
       
-      # The environments for which notifications can be posted
-      :enabled_environments
+        # The environments for which notifications can be posted
+        :enabled_environments
   
-  def initialize
-    self.protocol = "https"
-    self.host = "coalmineapp.com"
-    self.port = 443
-    self.secure = true
-    self.enabled_environments = ["production", "staging"]
+    def initialize
+      self.protocol = "https"
+      self.host = "coalmineapp.com"
+      self.port = 443
+      self.secure = true
+      self.enabled_environments = ["production", "staging"]
     
-    self.http_open_timeout = 3
-    self.http_read_timeout = 6
-    self.logger = Coalmine::Logger.new
-    self.filters = []
-  end
+      self.http_open_timeout = 3
+      self.http_read_timeout = 6
+      self.logger = Coalmine::Logger.new
+      self.filters = []
+    end
    
-  def protocol=(proto)
-    proto = "http" unless ["http", "https"].include?(proto)
-    @protocol = proto
-  end
+    def protocol=(proto)
+      proto = "http" unless ["http", "https"].include?(proto)
+      @protocol = proto
+    end
   
-  def secure?
-    self.secure
+    def secure?
+      self.secure
+    end
   end
 end
