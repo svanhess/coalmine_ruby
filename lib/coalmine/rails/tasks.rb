b2aba7db4b91b8d0bcdeb2890290b6effe178f8f
Brad Seefeld <bradley.seefeld@gmail.com>
2012-04-29 14:51:48 -0700

    Add rake task to manually send a test notification to Coalmine

--- lib/coalmine/rails/tasks.rb
+++ lib/coalmine/rails/tasks.rb
@@ -10,4 +10,14 @@ namespace :coalmine do
       puts "There was an error while notifying Coalmine. Please check the logs for details."
     end
   end
+  
+  desc "Send a test notification to Coalmine"
+  task :test => :environment do
+    notification = Coalmine::Notification.new(:message => "Test notification")
+    if Coalmine.send(notification)
+      puts "Successfully sent notification to Coalmine"
+    else
+      puts "There was an error while notifying Coalmine. Please check the logs for details"
+    end
+  end
 end
--- lib/coalmine/version.rb
+++ lib/coalmine/version.rb
@@ -1,3 +1,3 @@
 module Coalmine
-  VERSION = "0.5.2"
+  VERSION = "0.5.3"
 end
